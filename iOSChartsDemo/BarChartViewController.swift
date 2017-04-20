55b8d9de65bc2590cf7b75d7ca355dff0179712d
Joseph <Ethan.Joseph@tdameritrade.com>
2017-04-20 11:10:10 -0400

    first commit, charts demo, bar chart added

---
+++ iOSChartsDemo/BarChartViewController.swift
@@ -0,0 +1,46 @@
+//
+//  BarChartViewController.swift
+//  iOSChartsDemo
+//
+//  Created by Joyce Echessa on 6/12/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class BarChartViewController: UIViewController {
+
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var months: [String]!
+    
+    func setChart(dataPoints: [String], values: [Double]){
+        barChartView.noDataText = "You need to provide data for the chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
+        
+        let chartData = BarChartData(dataSet: chartDataSet)
+        
+        barChartView.data = chartData
+        
+        barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: dataPoints)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(dataPoints: months, values: unitsSold)
+    }
+}
+
