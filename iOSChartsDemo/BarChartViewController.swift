336ac07757775ef63f367e00679a3d2c868e8efd
Joseph <Ethan.Joseph@tdameritrade.com>
2017-04-20 11:38:40 -0400

    Added new cell to table view and viewController for projection chart

--- iOSChartsDemo/BarChartViewController.swift
+++ iOSChartsDemo/BarChartViewController.swift
@@ -16,27 +16,35 @@ class BarChartViewController: UIViewController {
     var months: [String]!
     
     func setChart(dataPoints: [String], values: [Double]){
+        
+        //Label if data does not load
         barChartView.noDataText = "You need to provide data for the chart."
         
         var dataEntries: [BarChartDataEntry] = []
         
+        //Build Entries
         for i in 0..<dataPoints.count {
             let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
             dataEntries.append(dataEntry)
         }
         
+        //Build Data Set
         let chartDataSet = BarChartDataSet(values: dataEntries, label: "Units Sold")
         
+        //Build Data
         let chartData = BarChartData(dataSet: chartDataSet)
         
+        //Set Chart Data
         barChartView.data = chartData
         
+        //Axis Formatter
         barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: dataPoints)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Mock Data
         months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         
---
+++ iOSChartsDemo/ProjectionChartViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Charts
+
+class ProjectionChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
