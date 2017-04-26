f086288c92b52aa71e0535192cc18536edacba12
Joseph <Ethan.Joseph@tdameritrade.com>
2017-04-26 16:04:11 -0400

    EJOS created label renderer that draws axis label bubbles and text

---
+++ iOSChartsDemo/LabelRenderer.swift
@@ -0,0 +1,327 @@
+//
+//  LabelRenderer.swift
+//  iOSChartsDemo
+//
+//  Created by Joseph, Ethan on 4/26/17.
+//  Copyright © 2017 Appcoda. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+import Charts
+
+//@objc(ChartYAxisRenderer)
+open class LabelRenderer: YAxisRenderer
+{
+    public override init(viewPortHandler: ViewPortHandler?, yAxis: YAxis?, transformer: Transformer?)
+    {
+        super.init(viewPortHandler: viewPortHandler, yAxis: yAxis, transformer: transformer)
+    }
+    
+    /// draws the y-axis labels to the screen
+    open override func renderAxisLabels(context: CGContext)
+    {
+        guard
+            let yAxis = self.axis as? YAxis,
+            let viewPortHandler = self.viewPortHandler
+            else { return }
+        
+        if !yAxis.isEnabled || !yAxis.isDrawLabelsEnabled
+        {
+            return
+        }
+        
+        let xoffset = yAxis.xOffset
+        let yoffset = yAxis.labelFont.lineHeight / 2.5 + yAxis.yOffset
+        
+        let dependency = yAxis.axisDependency
+        let labelPosition = yAxis.labelPosition
+        
+        //mine
+        var xPos  = viewPortHandler.contentRight
+        
+        var textAlign: NSTextAlignment
+        
+        if dependency == .left
+        {
+            if labelPosition == .outsideChart
+            {
+                textAlign = .right
+                xPos = viewPortHandler.offsetLeft - xoffset
+            }
+            else
+            {
+                textAlign = .left
+                xPos = viewPortHandler.offsetLeft + xoffset
+            }
+            
+        }
+        else
+        {
+            if labelPosition == .outsideChart
+            {
+                textAlign = .left
+                xPos = viewPortHandler.contentRight + xoffset
+            }
+            else
+            {
+                textAlign = .right
+                xPos = viewPortHandler.contentRight - xoffset
+            }
+        }
+        
+        drawYLabelsPublic(
+            context: context,
+            fixedPosition: xPos,
+            positions: transformedPositions(),
+            offset: yoffset, //yoffset - yAxis.labelFont.lineHeight,
+            textAlign: textAlign)
+        }
+    
+    open override func renderAxisLine(context: CGContext)
+    {
+        guard
+            let yAxis = self.axis as? YAxis,
+            let viewPortHandler = self.viewPortHandler
+            else { return }
+        
+        if !yAxis.isEnabled || !yAxis.drawAxisLineEnabled
+        {
+            return
+        }
+        
+        context.saveGState()
+        
+        context.setStrokeColor(yAxis.axisLineColor.cgColor)
+        context.setLineWidth(yAxis.axisLineWidth)
+        if yAxis.axisLineDashLengths != nil
+        {
+            context.setLineDash(phase: yAxis.axisLineDashPhase, lengths: yAxis.axisLineDashLengths)
+        }
+        else
+        {
+            context.setLineDash(phase: 0.0, lengths: [])
+        }
+        
+        if yAxis.axisDependency == .left
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: viewPortHandler.contentLeft, y: viewPortHandler.contentTop))
+            context.addLine(to: CGPoint(x: viewPortHandler.contentLeft, y: viewPortHandler.contentBottom))
+            context.strokePath()
+        }
+        else
+        {
+            context.beginPath()
+            context.move(to: CGPoint(x: viewPortHandler.contentRight, y: viewPortHandler.contentTop))
+            context.addLine(to: CGPoint(x: viewPortHandler.contentRight, y: viewPortHandler.contentBottom))
+            context.strokePath()
+        }
+        
+        context.restoreGState()
+    }
+    
+    /// draws the y-labels on the specified x-position
+    public func drawYLabelsPublic(
+        context: CGContext,
+        fixedPosition: CGFloat,
+        positions: [CGPoint],
+        offset: CGFloat,
+        textAlign: NSTextAlignment)
+    {
+        guard
+            let yAxis = self.axis as? YAxis
+            else { return }
+        
+        let labelFont = yAxis.labelFont
+        
+        //let labelTextColor = yAxis.labelTextColor
+        let labelTextColor = UIColor.white
+        
+        let from = yAxis.isDrawBottomYLabelEntryEnabled ? 0 : 1
+        let to = yAxis.isDrawTopYLabelEntryEnabled ? yAxis.entryCount : (yAxis.entryCount - 1)
+        
+        for i in stride(from: from, to: to, by: 1)
+        {
+            //This is the label that the formatter we need to create will return
+            let text = yAxis.getFormattedLabel(i)
+            
+            let image = #imageLiteral(resourceName: "y-axis-bubble")
+            
+            let size = CGSize(width: 50.0, height: 20.0)
+            
+            //there's this draw Image thing n b
+            ChartUtils.drawImage(context: context, image: image, x: fixedPosition + 25, y: positions[i].y + offset + 5, size: size)
+            
+            ChartUtils.drawText(
+                context: context,
+                text: text,
+                point: CGPoint(x: fixedPosition + 10, y: positions[i].y + offset),
+                align: textAlign,
+                attributes: [NSFontAttributeName: labelFont, NSForegroundColorAttributeName: labelTextColor])
+        }
+    }
+    
+    open override func renderGridLines(context: CGContext)
+    {
+        guard let
+            yAxis = self.axis as? YAxis
+            else { return }
+        
+        if !yAxis.isEnabled
+        {
+            return
+        }
+        
+        if yAxis.drawGridLinesEnabled
+        {
+            let positions = transformedPositions()
+            
+            context.saveGState()
+            defer { context.restoreGState() }
+            context.clip(to: self.gridClippingRect)
+            
+            context.setShouldAntialias(yAxis.gridAntialiasEnabled)
+            context.setStrokeColor(yAxis.gridColor.cgColor)
+            context.setLineWidth(yAxis.gridLineWidth)
+            context.setLineCap(yAxis.gridLineCap)
+            
+            if yAxis.gridLineDashLengths != nil
+            {
+                context.setLineDash(phase: yAxis.gridLineDashPhase, lengths: yAxis.gridLineDashLengths)
+                
+            }
+            else
+            {
+                context.setLineDash(phase: 0.0, lengths: [])
+            }
+            
+            // draw the grid
+            for i in 0 ..< positions.count
+            {
+                drawGridLine(context: context, position: positions[i])
+            }
+        }
+        
+        if yAxis.drawZeroLineEnabled
+        {
+            // draw zero line
+            drawZeroLine(context: context)
+        }
+    }
+    
+    open override func renderLimitLines(context: CGContext)
+    {
+        guard
+            let yAxis = self.axis as? YAxis,
+            let viewPortHandler = self.viewPortHandler,
+            let transformer = self.transformer
+            else { return }
+        
+        var limitLines = yAxis.limitLines
+        
+        if limitLines.count == 0
+        {
+            return
+        }
+        
+        context.saveGState()
+        
+        let trans = transformer.valueToPixelMatrix
+        
+        var position = CGPoint(x: 0.0, y: 0.0)
+        
+        for i in 0 ..< limitLines.count
+        {
+            let l = limitLines[i]
+            
+            if !l.isEnabled
+            {
+                continue
+            }
+            
+            context.saveGState()
+            defer { context.restoreGState() }
+            
+            var clippingRect = viewPortHandler.contentRect
+            clippingRect.origin.y -= l.lineWidth / 2.0
+            clippingRect.size.height += l.lineWidth
+            context.clip(to: clippingRect)
+            
+            position.x = 0.0
+            position.y = CGFloat(l.limit)
+            position = position.applying(trans)
+            
+            context.beginPath()
+            context.move(to: CGPoint(x: viewPortHandler.contentLeft, y: position.y))
+            context.addLine(to: CGPoint(x: viewPortHandler.contentRight, y: position.y))
+            
+            context.setStrokeColor(l.lineColor.cgColor)
+            context.setLineWidth(l.lineWidth)
+            if l.lineDashLengths != nil
+            {
+                context.setLineDash(phase: l.lineDashPhase, lengths: l.lineDashLengths!)
+            }
+            else
+            {
+                context.setLineDash(phase: 0.0, lengths: [])
+            }
+            
+            context.strokePath()
+            
+            let label = l.label
+            
+            // if drawing the limit-value label is enabled
+            if l.drawLabelEnabled && label.characters.count > 0
+            {
+                let labelLineHeight = l.valueFont.lineHeight
+                
+                let xOffset: CGFloat = 4.0 + l.xOffset
+                let yOffset: CGFloat = l.lineWidth + labelLineHeight + l.yOffset
+                
+                if l.labelPosition == .rightTop
+                {
+                    ChartUtils.drawText(context: context,
+                                        text: label,
+                                        point: CGPoint(
+                                            x: viewPortHandler.contentRight - xOffset,
+                                            y: position.y - yOffset),
+                                        align: .right,
+                                        attributes: [NSFontAttributeName: l.valueFont, NSForegroundColorAttributeName: l.valueTextColor])
+                }
+                else if l.labelPosition == .rightBottom
+                {
+                    ChartUtils.drawText(context: context,
+                                        text: label,
+                                        point: CGPoint(
+                                            x: viewPortHandler.contentRight - xOffset,
+                                            y: position.y + yOffset - labelLineHeight),
+                                        align: .right,
+                                        attributes: [NSFontAttributeName: l.valueFont, NSForegroundColorAttributeName: l.valueTextColor])
+                }
+                else if l.labelPosition == .leftTop
+                {
+                    ChartUtils.drawText(context: context,
+                                        text: label,
+                                        point: CGPoint(
+                                            x: viewPortHandler.contentLeft + xOffset,
+                                            y: position.y - yOffset),
+                                        align: .left,
+                                        attributes: [NSFontAttributeName: l.valueFont, NSForegroundColorAttributeName: l.valueTextColor])
+                }
+                else
+                {
+                    ChartUtils.drawText(context: context,
+                                        text: label,
+                                        point: CGPoint(
+                                            x: viewPortHandler.contentLeft + xOffset,
+                                            y: position.y + yOffset - labelLineHeight),
+                                        align: .left,
+                                        attributes: [NSFontAttributeName: l.valueFont, NSForegroundColorAttributeName: l.valueTextColor])
+                }
+            }
+        }
+        
+        context.restoreGState()
+    }
+}
--- iOSChartsDemo/ProjectionChartViewController.swift
+++ iOSChartsDemo/ProjectionChartViewController.swift
@@ -8,46 +8,47 @@ class ProjectionChartViewController: UIViewController {
     
     var projectionString : String?
     
-    class ProjectionLabelFormatter: DefaultAxisValueFormatter {
-        
-        //var asd = Projection.getDefaultProjection
-        
-        public var DEFAULT_PROJECTION = [95, 90, 70, 50, 30, 10, 5]
-        
-        var mProjectionLabels : [Float]
-        
-        private var isShowingPercentAmount = false
-        
-        public init(mProjectionLabels : [Float]){
-            self.mProjectionLabels = mProjectionLabels
-            super.init()
-        }
-        
-        public func setIsShowingPercentAmount(isShowingPercentAmount: Bool){
-            self.isShowingPercentAmount = isShowingPercentAmount
-        }
-        
-        override func stringForValue(_ value: Double, axis: AxisBase?) -> String {
-            
-            //for now commenting this out so always show percent amount
-            //if(isShowingPercentAmount){
-                var index = 0
-                for i in 0..<mProjectionLabels.count{
-                    let floatVal = mProjectionLabels[i]
-                    if (floatVal == Float(value)){
-                        index = i
-                        break
-                    }
-                }
-                
-                return String(format: "%s%%", DEFAULT_PROJECTION[index])
-//            }else{
-//                return String(format: "$%s", <#T##arguments: CVarArg...##CVarArg#>)
-//            }
-        }
-    }
-
     
+    //This class is responsible for formatting the values passed into the new yAxis
+//    class ProjectionLabelFormatter: IAxisValueFormatter {
+//        
+//        //var asd = Projection.getDefaultProjection
+//        
+//        public var DEFAULT_PROJECTION = [95, 90, 70, 50, 30, 10, 5]
+//        
+//        var mProjectionLabels : [Float]
+//        
+//        private var isShowingPercentAmount = false
+//        
+//        public init(mProjectionLabels : [Float]){
+//            self.mProjectionLabels = mProjectionLabels
+//            super.init()
+//        }
+//        
+//        public func setIsShowingPercentAmount(isShowingPercentAmount: Bool){
+//            self.isShowingPercentAmount = isShowingPercentAmount
+//        }
+//        
+//        override func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+//            
+//            //for now commenting this out so always show percent amount
+//            //if(isShowingPercentAmount){
+//                var index = 0
+//                for i in 0..<mProjectionLabels.count{
+//                    let floatVal = mProjectionLabels[i]
+//                    if (floatVal == Float(value)){
+//                        index = i
+//                        break
+//                    }
+//                }
+//                
+//                return String(format: "%s%%", DEFAULT_PROJECTION[index])
+////            }else{
+////                return String(format: "$%s", <#T##arguments: CVarArg...##CVarArg#>)
+////            }
+//        }
+//    }
+        
     func setProjectionChart(projection: Projection, goal: Goal){
         
         projectionChartView.setExtraOffsets(left: 0, top: 0, right: 18, bottom: 0)
@@ -158,8 +159,12 @@ class ProjectionChartViewController: UIViewController {
         projectionChartView.data = projectionData
         projectionChartView.animate(xAxisDuration: 1.5, yAxisDuration: 1.5, easingOption: ChartEasingOption.easeInSine)
         
-        //I think this is to render the labels
-        //projectionChartView.rightYAxisRenderer
+        //Parameters for renderer
+        let viewPortHandler = projectionChartView.viewPortHandler
+        let yAxis = projectionChartView.getAxis(YAxis.AxisDependency.right)
+        let transformer = projectionChartView.getTransformer(forAxis: YAxis.AxisDependency.right)
+        
+        projectionChartView.rightYAxisRenderer = LabelRenderer(viewPortHandler: viewPortHandler, yAxis: yAxis, transformer: transformer)
     }
     
     func buildSplitBand(band: Band, yearsToGoal: Int) -> LineChartDataSet{
@@ -198,7 +203,7 @@ class ProjectionChartViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        projectionString = "{\"targetAnnualIncome\":null,\"startYear\":2016,\"isCurrentDollars\":true,\"bands\":[{\"percentile\":5,\"value\":[21223.071,21240.326571,20556.741981,21794.030241,22371.650895,22729.501234,23799.499513,23067.337084,24501.951792,25265.684211,26683.946455,26643.735893,26332.375831,29401.373277,30630.24683,32305.639077,32572.051165,32913.046886,36413.65465,37738.824872,39749.523055,41191.361405,41565.671905,43909.532747,44850.440552,46567.89654,47886.586539,47147.180357,0.0]},{\"percentile\":30,\"value\":[25256.45925,26222.428279,27617.450431,29351.831725,31114.914454,32480.694073,33983.122832,36130.967598,38133.663583,40148.904567,43259.178595,45379.51216,46719.16989,49634.935014,51433.145012,54839.971431,58427.050807,61567.505169,65432.451111,70502.111229,75014.496307,76880.101335,79533.171057,84213.342872,90012.150065,95821.632859,102856.272249,106957.519343,0.0]},{\"percentile\":50,\"value\":[26761.47325,28902.325185,31457.984437,33832.546863,35710.206463,38090.857872,40082.582694,43138.127444,44948.549802,47815.376304,53065.276308,56996.387252,59918.480504,64272.936456,66700.962056,69363.425749,73595.917583,78627.723103,84623.68198,90473.558356,95000.567228,102681.521907,105738.048873,112967.830457,117637.264195,123675.013215,131002.44484,141140.801294,0.0]},{\"percentile\":70,\"value\":[28593.2535,31525.034422,34865.474082,38191.999438,41204.684108,44429.715231,48618.349901,51599.865044,55243.340398,59903.057595,63257.952105,68744.909923,74630.94953,79429.404683,84787.245052,89183.455399,96141.78855,105148.214452,113810.916986,118704.942888,126365.95237,130752.713211,135457.911666,142439.158689,154635.136871,170200.815315,178851.222955,189138.051419,35369.950914]},{\"percentile\":90,\"value\":[31219.53925,34810.701518,39878.564684,45093.695967,49127.959075,54715.501433,60610.173,67566.109309,72912.153104,78710.162807,86811.885866,91463.491776,101068.118838,110943.303886,125663.641797,133028.285078,140157.765426,151279.268943,159602.728984,170899.177534,187225.644715,192167.317931,209599.31803,226727.864363,248375.11369,264656.690651,278052.791607,306082.028793,161555.251931]},{\"percentile\":95,\"value\":[32339.319,37214.932394,43206.500875,48738.848734,53187.226764,61022.77589,68122.621101,76980.478987,82689.281467,93833.539008,101853.005098,106952.519446,118803.779646,127103.389403,142658.595778,148911.169626,159697.029163,179229.004515,187767.528965,200957.053636,211054.807043,230039.582222,258068.697346,278780.087046,310574.364388,326333.040525,353825.85326,359885.116668,221075.297922]}],\"targetLumpSum\":157000,\"targetMonthlyIncome\":null,\"chanceOfSuccess\":26.5}"
+        projectionString = "{\"targetAnnualIncome\":null,\"startYear\":2016,\"isCurrentDollars\":true,\"bands\":[{\"percentile\":5,\"value\":[21223.071,21240.326571,20556.741981,21794.030241,22371.650895,22729.501234,23799.499513,23067.337084,24501.951792,25265.684211,26683.946455,26643.735893,26332.375831,29401.373277,30630.24683,32305.639077,32572.051165,32913.046886,36413.65465,37738.824872,39749.523055,41191.361405,41565.671905,43909.532747,44850.440552,46567.89654,47886.586539,47147.180357,0.0]},{\"percentile\":30,\"value\":[25256.45925,26222.428279,27617.450431,29351.831725,31114.914454,32480.694073,33983.122832,36130.967598,38133.663583,40148.904567,43259.178595,45379.51216,46719.16989,49634.935014,51433.145012,54839.971431,58427.050807,61567.505169,65432.451111,70502.111229,75014.496307,76880.101335,79533.171057,84213.342872,90012.150065,95821.632859,102856.272249,106957.519343,0.0]},{\"percentile\":50,\"value\":[26761.47325,28902.325185,31457.984437,33832.546863,35710.206463,38090.857872,40082.582694,43138.127444,44948.549802,47815.376304,53065.276308,56996.387252,59918.480504,64272.936456,66700.962056,69363.425749,73595.917583,78627.723103,84623.68198,90473.558356,95000.567228,102681.521907,105738.048873,112967.830457,117637.264195,123675.013215,131002.44484,141140.801294,0.0]},{\"percentile\":70,\"value\":[28593.2535,31525.034422,34865.474082,38191.999438,41204.684108,44429.715231,48618.349901,51599.865044,55243.340398,59903.057595,63257.952105,68744.909923,74630.94953,79429.404683,84787.245052,89183.455399,96141.78855,105148.214452,113810.916986,118704.942888,126365.95237,130752.713211,135457.911666,142439.158689,154635.136871,170200.815315,178851.222955,189138.051419,35369.950914]},{\"percentile\":90,\"value\":[31219.53925,34810.701518,39878.564684,45093.695967,49127.959075,54715.501433,60610.173,67566.109309,72912.153104,78710.162807,86811.885866,91463.491776,101068.118838,110943.303886,125663.641797,133028.285078,140157.765426,151279.268943,159602.728984,170899.177534,187225.644715,192167.317931,209599.31803,226727.864363,248375.11369,264656.690651,278052.791607,306082.028793,161555.251931]},{\"percentile\":95,\"value\":[32339.319,37214.932394,43206.500875,48738.848734,53187.226764,61022.77589,68122.621101,76980.478987,82689.281467,93833.539008,101853.005098,106952.519446,118803.779646,127103.389403,142658.595778,148911.169626,159697.029163,179229.004515,187767.528965,200957.053636,211054.807043,230039.582222,258068.697346,278780.087046,310574.364388,326333.040525,353825.85326,359885.116668,221075.297922]}],\"targetLumpSum\":157000,\"targetMonthlyIncome\":null,\"chanceOfSuccess\":40.5}"
         
         
         
